55f4c734431ef753024d4237d9c2e65c41fcbc70
Aprajita <mishraaprajita43@gmail.com>
2026-05-05 11:00:25 +0530

    react

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import LikeButton from "./components/LikeButton";
+import UserWebsites from "./components/UserWebsite";
+
+function App() {
+  return (
+    <div >
+      <h1>React Exercises</h1>
+      <LikeButton />
+      <hr />
+      <UserWebsites />
+    </div>
+  );
+}
+
+export default App;
